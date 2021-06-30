d19ea4d84a0bb9c605036e2c850acb174c6dc2e8
soumikmahato <soumikmahato@yahoo.com>
2021-06-30 10:40:01 +0530

    rtquery.js changes

--- src/rtquery.js
+++ src/rtquery.js
@@ -6,6 +6,19 @@ class rtquery{
     }
     
     render(elem, attrs, parent, html){
+        if(!elem || !attrs || !parent ){
+            return new Promise((success, failed)=>{
+                failed("parameters should not be empty");
+        });
+    } 
+    //if innerhtml not defined
+    if(!html){
+        html = "";
+      }
+        //to create Element and embed in parent
+        let node = document.createElement(elem);
+        parent.appendChild(node);
+        node.innerHTML = html;
         
     }
 }
